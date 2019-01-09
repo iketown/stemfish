9d5c31cfc601691b91e5f07b36c4388acfbb1bd7
ike76 <ike76@me.com>
2019-01-09 11:57:52 -0600

    setup with material-ui

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,11 +2,17 @@ import React, { Component } from "react";
 import { compose } from "redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { connect } from "react-redux";
+//
+import { Button } from "@material-ui/core";
+
 export class Home extends Component {
   render() {
     return (
       <div>
         <h3>home</h3>
+        <Button variant="contained" color="primary">
+          Hi there
+        </Button>
       </div>
     );
   }
